f425bddd99fe303fad0ebf83a3186fc6571b934c
Sai.Gaddam <saikumar.gaddam@cigniti.com>
2026-01-29 00:57:48 +0530

    introduced env vars

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
 
 print('Hello from python executing inside Jenkins')
-print(f'APP_ENV is {os.getenv("APP_ENV")})')
-print(f'BUILD_OWNER is {os.getenv("BUILD_OWNER")})')
+print(f'APP_ENV is {os.getenv("APP_ENV")}')
+print(f'BUILD_OWNER is {os.getenv("BUILD_OWNER")}')

5ab6b0b634dd3eb15032892c6eb9a575f19c26db
Sai.Gaddam <saikumar.gaddam@cigniti.com>
2026-01-29 23:57:35 +0530

    removed error in negative test case

--- tests/test_login_negative.py
+++ tests/test_login_negative.py
@@ -3,4 +3,4 @@ from playwright.sync_api import sync_playwright
 
 def test_login(page):
     page.goto("https://opensource-demo.orangehrmlive.com")
-    assert page.title() == "OrangeHRM<"
+    assert page.title() == "OrangeHRM"

bcd3c412f03112101c24b22e066169fd9b26bcd2
Sai.Gaddam <saikumar.gaddam@cigniti.com>
2026-01-29 16:48:36 +0530

    developed conftest.py fixtures and their usages

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,10 +1,6 @@
 from playwright.sync_api import sync_playwright
 
 
-def test_login():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context(viewport=None)
-        page = context.new_page()
-        page.goto("https://google.com")
-        print(page.title())
+def test_login(page):
+    page.goto("https://opensource-demo.orangehrmlive.com")
+    assert page.title() == "OrangeHRM"

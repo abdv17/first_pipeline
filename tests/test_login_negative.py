c58c5ad846c094ce993cca7fd120450dbc856b94
Sai.Gaddam <saikumar.gaddam@cigniti.com>
2026-01-31 00:19:12 +0530

    waiting for page to load fully

--- tests/test_login_negative.py
+++ tests/test_login_negative.py
@@ -2,5 +2,6 @@ from playwright.sync_api import sync_playwright
 
 
 def test_login_negative(page):
-    page.goto("https://opensource-demo.orangehrmlive.com")
+    page.goto("https://opensource-demo.orangehrmlive.com",wait_until="ready")
+    page.wait_for_timeout(5000)
     assert page.title() == "OrangeHRMa"

843fb64fbbada2c1ab1565f5864af5d354c69af0
Sai.Gaddam <saikumar.gaddam@cigniti.com>
2026-01-31 00:42:10 +0530

    developed smoke and regression cases

--- tests/test_login_negative.py
+++ tests/test_login_negative.py
@@ -4,5 +4,6 @@ from playwright.sync_api import sync_playwright
 @pytest.mark.smoke
 def test_login_username_password(page):
     page.goto("https://opensource-demo.orangehrmlive.com",wait_until="domcontentloaded")
+    page.wait_for_load_state('domcontentloaded')
     assert page.get_by_role('textbox',name='Username').is_visible()
     assert page.get_by_role('textbox',name='Password').is_visible()

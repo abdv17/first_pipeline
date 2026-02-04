f9a1de611a481e9b633b2d0d17bd72a8d8b7c8d1
abdv17 <sg8412@gmail.com>
2026-02-05 00:41:55 +0530

    added pytest html post processing

--- tests/test_login_button.py
+++ tests/test_login_button.py
@@ -1,7 +1,7 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
-@pytest.mark.regression
+@pytest.mark.smoke
 def test_login_username_password(page):
     page.goto("https://opensource-demo.orangehrmlive.com",wait_until="domcontentloaded")
     assert page.get_by_role('button',name='Login').is_visible()

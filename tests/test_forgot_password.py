72f4997e34619d672dd755df6f9f49fbfd697b08
abdv17 <sg8412@gmail.com>
2026-02-07 01:18:59 +0530

    marked all test cases as smoke

--- tests/test_forgot_password.py
+++ tests/test_forgot_password.py
@@ -1,7 +1,11 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
-@pytest.mark.regression
+from pages.login_page import LoginPage
+
+
+@pytest.mark.smoke
 def test_forgot_password(page):
-    page.goto("https://opensource-demo.orangehrmlive.com",wait_until="domcontentloaded")
+    login = LoginPage(page)
+    login.open()
     assert page.get_by_text("Forgot your password?", exact=True).is_visible()
